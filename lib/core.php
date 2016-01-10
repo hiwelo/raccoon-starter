d1bf9c04344bd08a694ee51d9bb164b3892cfbe5
Damien Senger <hi@hiwelo.co>
2016-01-10 19:36:19 +0100

    Core class – scripts & CSS & HTML injection methods

--- lib/core.php
+++ lib/core.php
@@ -39,4 +39,64 @@ class Core
             locate_template($library, true, true);
         }
     }
+
+    /**
+     * CSS injection in HTML HEAD
+     * @param  array  $element targeted DOM element
+     * @param  array  $css     CSS list
+     * @return void
+     */
+    static function injectStyle(array $element, array $css)
+    {
+        $element = implode('__', $element);
+        $output_begin = '<style>.' . $element . ' { ';
+        $output_content = [];
+        $output_end = ' } </style>';
+
+        foreach ($css as $attr => $value) {
+            $output_content[] = $attr . ': ' . $value . ';';
+        }
+
+        $output = $output_begin . implode(' ', $output_content) . $output_end;
+        echo $output;
+    }
+
+    /**
+     * HTML element injection in HTML DOM
+     * @param  string $type    HTML element to inject
+     * @param  array  $element HTML element class (row1__row2__...)
+     * @param  string $content HTML element content
+     * @return void
+     */
+    static function injectDOM($type, array $element, $content = '')
+    {
+        if ($element[0] === true) {
+            unset($element[0]);
+            $classes = '';
+            foreach ($element as $class) {
+                $classes .= implode('__', $class) . ' ';
+            }
+        } else {
+            $classes = implode('__', $element);
+        }
+
+        echo '<'.$type.' class="' . $classes . '">' . $content . '</' . $type . '>';
+    }
+
+    /**
+     * JS scripts injection in HTML DOM
+     * @param  string $file JS scripts list file
+     * @return void
+     */
+    static function injectScripts($file = 'scripts.json')
+    {
+        $file = file_get_contents(locate_template($file));
+        $json = json_decode($file);
+        foreach ($json->scripts as $script) {
+            $url = get_bloginfo('template_url') . '/' . $script;
+            $output .= '<script src="' . $url . '"></script>';
+        }
+
+        echo $output;
+    }
 }
