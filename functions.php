351211ece7a96cea706fb3c8d3af47a570cd0c4d
Damien Senger <hi@hiwelo.co>
2016-02-13 15:58:03 +0100

    Starter generic enhancements

--- functions.php
+++ functions.php
@@ -10,15 +10,3 @@
  * @license  https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License 3.0
  * @link     https://codex.wordpress.org/Functions_File_Explained
  */
-// use Hwlo\Raccoon\Raccoon;
-
-/*
- * PSR-4 autoloading system
- */
-// require 'vendor/autoload.php';
-
-/*
- * Raccoon WordPress theme setup
- */
-// Core::setup();
-// $raccoon = new Raccoon();
